d50868b7b8a2bcfdf3e75a6679d08055a085ecba
Nudara <nudaraf4@gmail.com>
2024-11-05 14:36:35 +1100

    recaptcha working

--- chipin/urls.py
+++ chipin/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
    path("", views.home, name="home"),
-   path('accounts/', include('allauth.urls')),
-   path("login/", views.login_view, name="login")
+   path('create_group/', views.create_group, name='create_group'),
+   path('group//', views.group_detail, name='group_detail'),
+   path('group//invite/', views.invite_users, name='invite_users'),
+   path('group//delete/', views.delete_group, name='delete_group'),
 ]
